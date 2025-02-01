04c40225d30a7572b3943eaa2c43b066e65e4388
vierimaa <o.vierimaa@gmail.com>
2025-02-01 14:25:53 +0200

    feat: data transform script transforms date fields, calculates duration, adds date based uuid and writes output to static json

--- scripts/transform-data.ts
+++ scripts/transform-data.ts
@@ -1,7 +1,7 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import Papa from 'papaparse';
+import fs from "fs";
+import path from "path";
+import Papa from "papaparse";
+import crypto from 'crypto';
 
 interface WorkoutData {
   title: string;
@@ -18,32 +18,60 @@ interface WorkoutData {
   distance_km: number | null;
   duration_seconds: number | null;
   rpe: number | null;
+  uuid: string;
 }
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const rawDataFilePath = path.join(__dirname, 'workouts.csv');
-
-function parseDate(dateString: string): string {
+const parseDate = (dateString: string): string => {
   const date = new Date(dateString);
   return date.toISOString(); // Converts to ISO 8601 format, e.g., "2024-10-01T17:02:00.000Z"
-}
+};
 
-fs.readFile(rawDataFilePath, 'utf-8', (err, rawData) => {
-  if (err) {
-    console.log('Error loading data:', err);
-    return; // Exit if there's an error
-  }
+const addDuration = (workout: WorkoutData): WorkoutData => {
+  const startTime = new Date(workout.start_time);
+  const endTime = new Date(workout.end_time);
+  const duration = endTime.getTime() - startTime.getTime();
+  return {
+    ...workout,
+    duration_seconds: duration / 1000,
+  };
+};
+
+const addUuid = (workout: WorkoutData): WorkoutData => {
+  const hash = crypto.createHash('sha256');
+  hash.update(workout.start_time);
+  const uuid = hash.digest('hex');
+  return {
+    ...workout,
+    uuid
+  };
+};
+
+const writeToJson = (data: WorkoutData[]): void => {
+  const json = JSON.stringify(data, null, 2);
+  const outputPath = path.join(process.cwd(), "static", "workoutData.json");
+  fs.writeFileSync(outputPath, json);
+};
+
+try {
+  const filePath = path.join(import.meta.dirname, "workouts.csv");
+  const rawData = fs.readFileSync(filePath, "utf-8");
 
   const parsedData = Papa.parse<WorkoutData>(rawData, {
-    header: true
+    header: true,
+  }).data;
+
+  const transformedData = parsedData.map((workout) => {
+    const withTransformedDate = {
+      ...workout,
+      start_time: parseDate(workout.start_time),
+      end_time: parseDate(workout.end_time),
+    };
+    const withDuration = addDuration(withTransformedDate);
+    return addUuid(withDuration);
   });
 
-  const data = parsedData.data
-
-  data.map(bar => console.log(bar))
-
-  // console.log(parsedData.data[9]);
-});
-
-// console.log("helloo")
+  writeToJson(transformedData);
+  console.info("Data transformation complete.");
+} catch (error) {
+  console.error("Error processing data:", error);
+}
